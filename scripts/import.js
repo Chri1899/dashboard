5c6b9d8ae24bf970aa7fd1af22e835635fba906c
Chri1899 <97324676+Chri1899@users.noreply.github.com>
2022-09-09 16:22:39 +0200

    Basic Markup Setup
    Co-authored-by: Tobias Wild <tobiaswild@users.noreply.github.com>

---
+++ scripts/import.js
@@ -0,0 +1,20 @@
+const header = document.querySelector("#header");
+fetch("components/header.html")
+  .then((res) => res.text())
+  .then((data) => {
+    header.innerHTML = data;
+  });
+
+const timeline = document.querySelector("#timeline");
+fetch("components/timeline.html")
+  .then((res) => res.text())
+  .then((data) => {
+    timeline.innerHTML = data;
+  });
+
+const andere = document.querySelector("#andere");
+fetch("components/andere.html")
+  .then((res) => res.text())
+  .then((data) => {
+    andere.innerHTML = data;
+  });
