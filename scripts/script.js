8f0c40440308e4e8f614cb8cf8a4e5bf9c5d692c
Tobias Wild <46871747+tobiaswild@users.noreply.github.com>
2022-09-12 11:38:28 +0200

    small design changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
 // function for showing and hiding the header profile dropdown
 function showDropdown() {
-  document.getElementById("myDropdown").classList.toggle("show");
+  document.getElementById("profileDropdown").classList.toggle("show");
 }
