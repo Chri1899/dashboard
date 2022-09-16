113ad578ea48fced2c8e25b5d4b3dcf5738cd446
Tobias Wild <46871747+tobiaswild@users.noreply.github.com>
2022-09-16 10:42:48 +0200

    remove change

--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,3 @@
 function showDropdown() {
   document.getElementById("profileDropdown").classList.toggle("show");
 }
-
-function change() {
-  var input = document.getElementById("input");
-  var progress = document.getElementById("test");
-  progress.value = input.value;
-  console.log(progress.value);
-}
