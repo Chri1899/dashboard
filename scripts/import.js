baff352cfc12ef6f3dd53fcce06e2beb403f7795
Tobias Wild <46871747+tobiaswild@users.noreply.github.com>
2022-10-10 11:32:27 +0200

    graph card

--- scripts/import.js
+++ scripts/import.js
@@ -1,27 +1,34 @@
 const header = document.querySelector("#header");
-fetch("components/globals/header.html")
+fetch("/components/globals/header.html")
   .then((res) => res.text())
   .then((data) => {
     header.innerHTML = data;
   });
 
-const timeline = document.querySelector("#timeline");
-fetch("components/index/timeline.html")
-  .then((res) => res.text())
-  .then((data) => {
-    timeline.innerHTML = data;
-  });
-
 const navigation = document.querySelector("#navigation");
-fetch("components/globals/navigation.html")
+fetch("/components/globals/navigation.html")
   .then((res) => res.text())
   .then((data) => {
     navigation.innerHTML = data;
   });
 
+const timeline = document.querySelector("#timeline");
+fetch("/components/index/timeline.html")
+  .then((res) => res.text())
+  .then((data) => {
+    timeline.innerHTML = data;
+  });
+
 const progress = document.querySelector("#progress");
-fetch("components/index/progress.html")
+fetch("/components/index/progress.html")
   .then((res) => res.text())
   .then((data) => {
     progress.innerHTML = data;
+  });
+
+const graph = document.querySelector("#graph");
+fetch("/components/index/graph.html")
+  .then((res) => res.text())
+  .then((data) => {
+    graph.innerHTML = data;
   });
